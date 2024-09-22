70bcc03a6e3484c88cc165f757a8ef041b2030f6
patriziapatti <patrizia.patti5@gmail.com>
2024-09-22 15:17:09 +0100

    service mail per edit e delete booking, get todayArrivals

--- backend/controllers/booking.controller.js
+++ backend/controllers/booking.controller.js
@@ -3,18 +3,18 @@ import Customer from '../models/customerSchema.js'
 import Room from '../models/roomSchema.js'
 import transport from '../services/mailService.js'
 
-export const getAllBookings = async (req,res)=>{
+export const getAllBookings = async (req, res) => {
     const page = req.query.page || 1
     let perPage = req.query.perPage || 5
     perPage = perPage > 10 ? 5 : perPage
     try {
         const allBookings = await Booking.find({})
-        .collation({locale: 'it'}) //serve per ignorare maiuscole e minuscole nell'ordine alfabetico del sort
-        .sort({customer:1}) 
-        .skip((page-1)*perPage)//salto la pagina precedente
-        .limit(perPage)
+            .collation({ locale: 'it' }) //serve per ignorare maiuscole e minuscole nell'ordine alfabetico del sort
+            .sort({ customer: 1 })
+            .skip((page - 1) * perPage)//salto la pagina precedente
+            .limit(perPage)
         const totalResults = await Booking.countDocuments()// mi da il numero totale di documenti
-        const totalPages = Math.ceil(totalResults / perPage )  
+        const totalPages = Math.ceil(totalResults / perPage)
         res.send({
             dati: allBookings,
             totalResults,
@@ -23,37 +23,33 @@ export const getAllBookings = async (req,res)=>{
 
         })
     } catch (error) {
-        res.status(404).send({message: 'Not Found'})
-    }  
+        res.status(404).send({ message: 'Not Found' })
+    }
 }
 
-export const getSingleBooking = async (req,res)=>{
-    const {id} =req.params
+export const getSingleBooking = async (req, res) => {
+    const { id } = req.params
     try {
         const booking = await Booking.findById(id)
-        res.status(200).send(booking) 
+        res.status(200).send(booking)
     } catch (error) {
-        res.status(404).send({message: 'Not Found'}) 
-    } 
+        res.status(404).send({ message: 'Not Found' })
+    }
 }
 
-export const addBooking = async (req,res)=>{
-   
-    // const booking = new Booking (req.body)
-    // const room = new Booking (req.body)
-    // const checkInDate =new Booking (req.body)
-    // const checkOutDate =new Booking (req.body)
-    const {customer, room, checkInDate, checkOutDate, status, pax} = req.body
+export const addBooking = async (req, res) => {
+
+    const { customer, room, checkInDate, checkOutDate, status, pax } = req.body
     let newBooking
     try {
         const roomData = await Room.findById(room)
         if (!roomData) {
-            return res.status(400).send({message: 'Room not Found'})
+            return res.status(400).send({ message: 'Room not Found' })
         }
-        const pricePerNight =roomData.price;
+        const pricePerNight = roomData.price;
         const checkIn = new Date(checkInDate)
         const checkOut = new Date(checkOutDate)
-        const numberOfNight= Math.ceil((checkOut-checkIn) / (1000 * 60 * 60 *24))
+        const numberOfNight = Math.ceil((checkOut - checkIn) / (1000 * 60 * 60 * 24))
         const totalPrice = pricePerNight * numberOfNight
         newBooking = new Booking({
             customer,
@@ -67,92 +63,151 @@ export const addBooking = async (req,res)=>{
         await newBooking.save()
         const formattedCheckInDate = checkIn.toLocaleDateString('en-GB'); // 'en-GB' per formato DD/MM/YYYY
         const formattedCheckOutDate = checkOut.toLocaleDateString('en-GB')
-    try {
-        const customer = await Customer.findById(newBooking.customer)
-        await transport.sendMail({
-            from: 'noreply@epicoders.com', // sender address
-            to: customer.email, // list of receivers
-            subject: "New Booking", // Subject line
-            text: `Dear ${customer.name}, Your Reservation from ${formattedCheckInDate} to ${formattedCheckOutDate} is confirmed! Total price is ${totalPrice}€, to pay at check-out.`, // plain text body
-            html: `<b>Dear ${customer.name}, <br> Your Reservation from ${formattedCheckInDate} to ${formattedCheckOutDate} is confirmed! Total price is ${totalPrice}€, to pay at check-out.<b>` // html body
-        })
-    } catch (emailError) {
-        console.error('Error sending email:', emailError);
-    }
-    res.status(200).send(newBooking)
-} catch (error) {
-    return res.status(400).send(error)
- }
-}
-
-export const editBooking = async (req,res)=>{
-    const {id} =req.params
-    try {
-        const booking = await Booking.findByIdAndUpdate(id, req.body, {new:true}) //new serve per restituire in author l'oggetto appena inserito, altrimenti non lo restituisce
-        await booking.save();
-      
-        res.status(200).send(booking)
+        try {
+            const customer = await Customer.findById(newBooking.customer)
+            await transport.sendMail({
+                from: 'noreply@epicoders.com', // sender address
+                to: customer.email, // list of receivers
+                subject: "New Booking", // Subject line
+                text: `Dear ${customer.name}, Your Reservation from ${formattedCheckInDate} to ${formattedCheckOutDate} is confirmed! Total price is ${totalPrice}€, to pay at check-out.`, // plain text body
+                html: `<b>Dear ${customer.name}, <br> Your Reservation from ${formattedCheckInDate} to ${formattedCheckOutDate} is confirmed! Total price is ${totalPrice}€, to pay at check-out.<b>` // html body
+            })
+        } catch (emailError) {
+            console.error('Error sending email:', emailError);
+        }
+        res.status(200).send(newBooking)
     } catch (error) {
-        res.status(400).send(error)
+        return res.status(400).send(error)
     }
-    
 }
-//TENTATIVO FALLITO DI INVIARE LA MAIL CON LA MODIFICA DELLA PRENOTAZIONE
-// export const editBooking = async (req,res)=>{
-//     const {id} =req.params
+
+export const editBooking = async (req, res) => {
+    const { id } = req.params;
+    try {
+        // Trova e popola la prenotazione esistente
+        const booking = await Booking.findById(id).populate('customer').populate('room');
+        if (booking) {
+            // Aggiorna la prenotazione con i nuovi dati e ripopola il campo room
+            const updatedBooking = await Booking.findByIdAndUpdate(id, req.body, { new: true }).populate('room');
+
+            // Ottieni il prezzo per notte della room associata
+            const pricePerNight = updatedBooking.room.price;
+
+            // Calcola il numero di notti e il prezzo totale
+            const checkInDate = new Date(updatedBooking.checkInDate);
+            const checkOutDate = new Date(updatedBooking.checkOutDate);
+            const nights = Math.ceil((checkOutDate - checkInDate) / (1000 * 60 * 60 * 24));
+            updatedBooking.totalPrice = nights * pricePerNight;
+
+            // Salva la prenotazione aggiornata
+            await updatedBooking.save();
+
+            // Prepara i dettagli per l'email
+            const customerName = booking.customer.name;
+            const customerEmail = booking.customer.email;
+
+            // Invia l'email di conferma
+            await transport.sendMail({
+                from: 'noreply@epicoders.com',
+                to: customerEmail,
+                subject: "Booking Updated",
+                text: `Dear ${customerName}, Your reservation has been updated. Here are the new details: Check-in from ${checkInDate.toLocaleDateString('en-GB')} to ${checkOutDate.toLocaleDateString('en-GB')}, total price: ${updatedBooking.totalPrice}€.`,
+                html: `<b>Dear ${customerName}, <br> Your reservation has been updated. Here are the new details: Check-in from ${checkInDate.toLocaleDateString('en-GB')} to ${checkOutDate.toLocaleDateString('en-GB')}, total price: ${updatedBooking.totalPrice}€.</b>`
+            });
+
+            res.status(200).send(updatedBooking);
+        } else {
+            res.status(404).send({ message: `Booking with ID ${id} not found` });
+        }
+    } catch (error) {
+        res.status(400).send({ message: `Error updating booking: ${error.message}` });
+    }
+};
+
+export const deleteBooking = async (req, res) => {
+    const { id } = req.params
+    try {
+        const booking = await Booking.findById(id).populate('customer');
+        //se l'id esiste nello schema allora fai la delete
+        if (await Booking.exists({ _id: id })) {
+            const customerEmail = booking.customer.email
+            const customerName = booking.customer.name
+            const checkInDate = booking.checkInDate.toLocaleDateString('en-GB')
+            const checkOutDate = booking.checkOutDate.toLocaleDateString('en-GB')
+            await Booking.findByIdAndDelete(id)
+            try {
+                await transport.sendMail({
+                    from: `noreply@epicoders.com`, // sender address
+                    to: customerEmail, // list of receivers
+                    subject: "New Booking", // Subject line
+                    text: `Dear ${customerName}, Your Reservation from ${checkInDate} to ${checkOutDate} is cancelled.`, // plain text body
+                    html: `<b>Dear ${customerName}, <br> Your Reservation from ${checkInDate} to ${checkOutDate} is cancelled.<b>` // html body
+                })
+
+            } catch (emailError) {
+                console.error('Error sending email:', emailError)
+            }
+            res.status(200).send(`ho eliminato la prenotazione con id: ${id}`)
+        } else { res.status(404).send({ message: `ID ${id} not found` }) }
+    } catch (error) {
+        res.status(404).send({ message: `ID ${id} not found` })
+    }
+}
+//ritorna 200 ma array vuoto
+// export const getTodaysArrivals = async (req, res) => {
 //     try {
-//         let booking = await Booking.findByIdAndUpdate(id)
-        
-//         if(!booking) {
-//             return res.status(404).send({message: 'Booking not found'})
-//         }
+//         // Ottieni la data odierna senza l'orario
+//         const today = new Date();
+//         today.setHours(0, 0, 0, 0);
 
-//         booking = Object.assign(booking, req.body)//aggiorno i campi della prenetazione con i nuovi dati
-//         if (req.body.checkInDate || req.body.checkOutDate || req.body.room){
-//             const roomData = await Room.findById(booking.room);
-//             if (!roomData) {
-//                 return res.status(400).send({ message: 'Room not found' });
-//             }
-//         }
-        
-//         const pricePerNight = roomData.price;
-//         const checkInDate =new Date(booking.checkInDate)
-//         const checkOutDate = new Date(booking.checkOutDate)
-//         const numberOfNights = Math.ceil((checkOutDate- checkInDate) / (1000 * 60 * 60 * 24));
-        
-//         booking.totalPrice = pricePerNight * numberOfNights;
+//         // Trova le prenotazioni in arrivo, in corso e in partenza oggi
+//         const bookingsArrivingToday = await Booking.find({ checkInDate: today }).populate('customer').populate('room');
+//         // const bookingsInHouseToday = await Booking.find({ checkInDate: { $lt: today }, checkOutDate: { $gt: today } }).populate('customer').populate('room');
+//         // const bookingsDepartingToday = await Booking.find({ checkOutDate: today }).populate('customer').populate('room');
 
+//         // Rispondi con i risultati trovati
+//         res.status(200).json({
+//             arrivingToday: bookingsArrivingToday,
+//             // inHouseToday: bookingsInHouseToday,
+//             // departingToday: bookingsDepartingToday
+//         });
+//     } catch (error) {
+//         res.status(500).send({ message: `Error fetching bookings: ${error.message}` });
+//     }
+// };
+
+export const getTodaysArrivals = async (req, res) => {
+    try {
+        // Ottieni la data odierna senza l'orario
+        const startOfDay = new Date();
+        startOfDay.setHours(0, 0, 0, 0);
+
+        // Imposta la fine del giorno
+        const endOfDay = new Date();
+        endOfDay.setHours(23, 59, 59, 999);
+
+        // Trova le prenotazioni in arrivo oggi
+        const bookingsArrivingToday = await Booking.find({
+            checkInDate: { $gte: startOfDay, $lte: endOfDay }
+        }).populate('customer').populate('room');
+
+        // Rispondi con i risultati trovati
+        res.status(200).json({
+            arrivingToday: bookingsArrivingToday,
+        });
+    } catch (error) {
+        res.status(500).send({ message: `Error fetching bookings: ${error.message}` });
+    }
+};
+// export const editBooking = async (req, res) => {
+//     const { id } = req.params
+//     try {
+//         const booking = await Booking.findByIdAndUpdate(id, req.body, { new: true }) //new serve per restituire in author l'oggetto appena inserito, altrimenti non lo restituisce
 //         await booking.save();
-        
-//         try {
-//             const customer = await Customer.findById(booking.customer);
-//             await transport.sendMail({
-//                 from: 'noreply@epicoders.com',
-//                 to: customer.email,
-//                 subject: "Booking Updated",
-//                 text: `Dear ${customer.name}, Your reservation has been updated. Here's the new details: Check in from ${checkInDate.toLocaleDateString('en-GB')} to ${checkOutDate.toLocaleDateString('en-GB')}, total price: ${booking.totalPrice}€.`,
-//                 html: `<b>Dear ${customer.name}, <br> Your reservation has been updated. Here's the new details: Check in from ${checkInDate.toLocaleDateString('en-GB')} to ${checkOutDate.toLocaleDateString('en-GB')}, total price: ${booking.totalPrice}€.<b>`
-//             })
-//         } catch (emailError) {
-//             console.error('Error sending email:', emailError);
-//         }
-      
+
 //         res.status(200).send(booking)
 //     } catch (error) {
 //         res.status(400).send(error)
 //     }
-// }
 
-export const deleteBooking = async (req,res)=>{
-    const {id} =req.params
-    try {
-        //se l'id esiste nello schema allora fai la delete
-        if (await Booking.exists({_id:id})){
-            await Booking.findByIdAndDelete(id)
-            res.status(200).send(`ho eliminato la prenotazione con id: ${id}`)
-        }else {res.status(404).send({message: `ID ${id} not found`})}
-        
-    } catch (error) {
-        res.status(404).send({message: `ID ${id} not found`})
-    }
-}
+// }
--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,14 +1,16 @@
 import express from 'express';
-import { addBooking, deleteBooking, editBooking, getAllBookings, getSingleBooking } from '../controllers/booking.controller.js';
+import { addBooking, deleteBooking, editBooking, getAllBookings, getSingleBooking, getTodaysArrivals } from '../controllers/booking.controller.js';
 
 
 const bookingRouter = express.Router()
 
+bookingRouter.get('/todayarr', getTodaysArrivals)
 bookingRouter.get('/', getAllBookings)
 bookingRouter.get('/:id',getSingleBooking)
 bookingRouter.post('/', addBooking)
 bookingRouter.put ('/:id', editBooking)
 bookingRouter.delete ('/:id', deleteBooking)
+
 
 
 export default bookingRouter
